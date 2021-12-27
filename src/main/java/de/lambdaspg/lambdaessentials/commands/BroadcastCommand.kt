9d3b66de31ac61b41a0681b27ec486197130482a
LambdaSpg <Lambda-spg@protonmail.com>
2021-12-27 12:06:43 +0100

    Changed Messages

--- src/main/java/de/lambdaspg/lambdaessentials/commands/BroadcastCommand.kt
+++ src/main/java/de/lambdaspg/lambdaessentials/commands/BroadcastCommand.kt
@@ -1,9 +1,11 @@
 package de.lambdaspg.lambdaessentials.commands
 
+import de.lambdaspg.lambdaessentials.MessageManager
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class BroadcastCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -16,7 +18,7 @@ class BroadcastCommand : CommandExecutor {
             Bukkit.broadcastMessage("»")
             Bukkit.broadcastMessage("§c[§4§lBROADCAST§c]: §4$message")
             Bukkit.broadcastMessage("»")
-        }
+        }else MessageManager.noPermission(sender as Player)
         return false
     }
 }
